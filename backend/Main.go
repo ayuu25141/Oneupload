06c4e8ce892d964eb7e440ea21a6edc8e68188f8
Ayush Chauhan <161006131+ayuu25141@users.noreply.github.com>
2025-12-31 13:00:42 +0530

    Update Main.go

--- backend/Main.go
+++ backend/Main.go
@@ -41,7 +41,7 @@ if port == "" {
 	// Fiber app
 	app := fiber.New()
 app.Use(cors.New(cors.Config{
-    AllowOrigins: "http://localhost:5173,http://localhost:3000",
+    AllowOrigins: "https://oneuploadme.vercel.app/,https://oneuploadme.vercel.app/",
     AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
     AllowHeaders: "Origin, Content-Type, Accept, Authorization",
     AllowCredentials: true,
